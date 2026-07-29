8040932200a5176c00e257a6f38a2ef09c1ab2db
Md Danish <mddanish4300@gmail.com>
2026-07-29 14:24:07 +0530

    Time: 0 ms (100%), Space: 43.3 MB (78.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        // Count the number of 0s, 1s, and 2s
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zero++;
+            } else if (nums[i] == 1) {
+                one++;
+            } else {
+                two++;
+            }
+        }
+
+        // Overwrite the array
+        int index = 0;
+
+        while (zero > 0) {
+            nums[index] = 0;
+            index++;
+            zero--;
+        }
+
+        while (one > 0) {
+            nums[index] = 1;
+            index++;
+            one--;
+        }
+
+        while (two > 0) {
+            nums[index] = 2;
+            index++;
+            two--;
+        }
+    }
+}
