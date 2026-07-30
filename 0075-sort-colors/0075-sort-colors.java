79c7c2bfc5324706e11b76f2ed10a73e24090720
Md Danish <mddanish4300@gmail.com>
2026-07-30 19:27:33 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,40 +1,31 @@
 class Solution {
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public void sortColors(int[] nums) {
 
-        int zero = 0;
-        int one = 0;
-        int two = 0;
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
 
-        // Count the number of 0s, 1s, and 2s
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zero++;
-            } else if (nums[i] == 1) {
-                one++;
-            } else {
-                two++;
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
             }
-        }
-
-        // Overwrite the array
-        int index = 0;
-
-        while (zero > 0) {
-            nums[index] = 0;
-            index++;
-            zero--;
-        }
-
-        while (one > 0) {
-            nums[index] = 1;
-            index++;
-            one--;
-        }
-
-        while (two > 0) {
-            nums[index] = 2;
-            index++;
-            two--;
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums, mid, high);
+                high--;
+            }
         }
     }
 }
