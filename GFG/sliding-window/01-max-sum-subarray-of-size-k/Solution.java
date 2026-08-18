e9bd6e00e7dacdec7f601ac0c2b5c61218ba230e
Md Danish <mddanish4300@gmail.com>
2026-08-18 17:32:38 +0530

    Update Solution.java

--- GFG/sliding-window/01-max-sum-subarray-of-size-k/Solution.java
+++ GFG/sliding-window/01-max-sum-subarray-of-size-k/Solution.java
@@ -1,4 +1,4 @@
- class Solution {
+class Solution {
     public int maxSubarraySum(int[] arr, int k) {
         // Code here
         int n = arr.length;
@@ -8,38 +8,33 @@
             return 0;
         }
         
-        int windowSum = 0;
+        int currWindowSum = 0;
         
-        // Calculate the first window
-        for (int i = 0; i < k; i++) {
-            windowSum += arr[i];
+        // 1. Calculate the sum of first window.
+        for(int i = 0; i < k; i++) {
+            currWindowSum += arr[i];
         }
         
+        int maxSum = currWindowSum;
+        
+        // Pointers for the current window.
         int low = 0;
         int high = k - 1;
-        int maxSum = Integer.MIN_VALUE;
         
-        // Slide the window
-        while (high < n) {
+        // 2. Slide the window
+        while (high < n - 1) {
+            
+            // Remove the element leaving the window.
+            currWindowSum -= arr[low];
+            
+            low++;  // Move the low pointer.
+            high++; // Move the high pointer.
+            
+            // Add the element entering the window
+            currWindowSum += arr[high];
             
             // Update the maximum
-            maxSum = Math.max(windowSum, maxSum);
-            
-            // Move the pointer
-            low++;
-            high++;
-            
-            // If high reaches the end, no, now no new element to add
-            if (high == n) {
-                break;
-            }
-            
-            // Remove the element leaving the window
-            windowSum = windowSum - arr[low - 1];
-            
-            
-            // Add the new element entering the window
-            windowSum = windowSum + arr[high];
+            maxSum = Math.max(currWindowSum, maxSum);
         }
         
         return maxSum;
