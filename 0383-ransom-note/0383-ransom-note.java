77d0727925abab5d48d81945ce979ec265390f1c
Md Danish <mddanish4300@gmail.com>
2026-09-04 21:08:15 +0530

    Time: 16 ms (19.42%), Space: 46.9 MB (34.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        HashMap<Character, Integer> need = new HashMap<>();
-        HashMap<Character, Integer> have = new HashMap<>();
+        // Firstly, created HashMap.
+        Map <Character, Integer> need = new HashMap<>();
+        Map <Character, Integer> have = new HashMap<>();
 
-        // Store frequency of characters required by ransomNote
+        // Store the frequency of characters required by ransomNote
         for (int i = 0; i < ransomNote.length(); i++) {
             char ch = ransomNote.charAt(i);
 
             need.put(ch, need.getOrDefault(ch, 0) + 1);
         }
-
-        // Store frequency of characters available in magazine
+        
+        // Store the frequency of characters available in magazine
         for (int i = 0; i < magazine.length(); i++) {
             char ch = magazine.charAt(i);
-            
+
             have.put(ch, have.getOrDefault(ch, 0) + 1);
         }
 
-        // Check whether magazine has enough of every required character
+        // Compare each characters with available characters.
         for (Map.Entry<Character, Integer> entry : need.entrySet()) {
             char ch = entry.getKey();
             int required = entry.getValue();
