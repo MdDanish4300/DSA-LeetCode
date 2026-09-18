693df88c8092762eef92e10e21a693fbaa8d8bc7
Md Danish <mddanish4300@gmail.com>
2026-09-19 00:33:44 +0530

    Time: 1 ms (99.81%), Space: 69.4 MB (11.67%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -7,8 +7,10 @@ class Solution {
         int minLenWindow = Integer.MAX_VALUE;
 
         for(int high = 0; high < n; high++) {
+            // Find the currSum and increase the window size.
             currSum += nums[high];
 
+            // Shrink the window while condition is satisfied.
             while (currSum >= target) {
                 int currWindowLen = high - low + 1;
 
